2831c65d0d6d75cc470ebb47e8f0dfa3c4cbf378
oi2in-dev <oi2ingaming@gmail.com>
2023-03-20 20:23:25 +0000

    Update 1.2, pending intent based on time added set at 15 seconds

--- app/src/main/java/com/example/e_tivity_3_intents/MainActivity.java
+++ app/src/main/java/com/example/e_tivity_3_intents/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.e_tivity_3_intents;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createnotifchannel();
 
         Title = findViewById(R.id.Title);
         Content =  findViewById(R.id.content);
@@ -57,7 +62,18 @@ public class MainActivity extends AppCompatActivity {
 
         btn3.setOnClickListener(v -> {
 
+            Toast.makeText(this, "ping sent", Toast.LENGTH_SHORT).show();
 
+            Intent intent = new Intent(MainActivity.this,BroadcastPing.class);
+            PendingIntent pendingIntent1 = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+            long timeAtButtonPressed = System.currentTimeMillis();
+
+            long fifteenSecs = 1000 * 15;
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP, timeAtButtonPressed + fifteenSecs, pendingIntent1);
         });
 
     }
@@ -76,5 +92,19 @@ public class MainActivity extends AppCompatActivity {
         NotificationCompat.Builder nb = NManager.getchan2ping(title, message);
         NManager.getAdmin().notify(2, nb.build());
 
+    }
+
+    private void createnotifchannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "PingMe";
+            String description = "Ping me in 15 secs";
+            NotificationChannel channel3 = new NotificationChannel("pingme", name, android.app.NotificationManager.IMPORTANCE_HIGH);
+            channel3.setDescription(description);
+
+            android.app.NotificationManager notificationManager = getSystemService(android.app.NotificationManager.class);
+            ((android.app.NotificationManager) notificationManager).createNotificationChannel(channel3);
+
+        }
     }
 }
